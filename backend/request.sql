9b813e0128dedd6ca09ff9cc4092fbd72bcd2bea
Guillaume-FAURE <faureg0717@gmail.com>
2022-03-25 12:05:37 +0100

    feat(sql): add recipes request

--- backend/request.sql
+++ backend/request.sql
@@ -19,3 +19,15 @@ INSERT INTO objectif (personne_id) SELECT personne.Personne_Id FROM personne
 UPDATE objectif
 SET calorie=$calorie, proteines=$proteines, glucides=$glucides, lipides=$lipides, sucres=$sucres, fibres=$fibres, gras_sature=$grasSature, cholesterol=$cholesterol, sel=$sel
 WHERE personne_id=$id
+
+-- composition from a food : {calorie, eau, proteines, glucides, lipides, sucres, fibres, gras_sature, cholesterol, sel}
+SELECT valeur FROM composition WHERE composition.aliments_Id=$id
+
+-- get the food list to pick on this list from the frontend
+SELECT * FROM aliments 
+
+-- get the food from a recipe
+SELECT aliments.id FROM aliments JOIN recette ON recette.aliments_enfants_id = aliments.id WHERE recette.aliments_parents_id = $recetteId
+
+-- add a new recipe
+INSERT INTO recette (food_id, ratio) VALUES ($food, $foodRatio)
