519e80127d72432f968190530901a43df83e9b52
Guillaume-FAURE <faureg0717@gmail.com>
2022-03-25 10:40:22 +0100

    feat(sql): add persons request

--- backend/request.sql
+++ backend/request.sql
@@ -1,1 +1,12 @@
 -- ALL THE SQL WE WILL NEED
+
+-- add a new person to the db
+INSERT INTO personne (Email, nom, prenom, date, password) VALUES ($email, $nom, $prenom, $date, $password)
+
+-- delete person from the db
+DELETE FROM personne WHERE Personne_id=$id
+
+-- update person in the db
+UPDATE personne
+SET Email=$Email, nom=$nom, prenom=$prenom, date=$date, password=$password
+WHERE Personne_id=$id
