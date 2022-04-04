7991bcf55b9d90bbe32cdbe0dbd0370851e1ba98
Guillaume-FAURE <faureg0717@gmail.com>
2022-04-04 16:11:26 +0200

    final update dashboard

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -1,68 +1,79 @@
 const id = sessionStorage.getItem("id");
-getGoal(id);
+let date = new Date();
+date = today();
 
 //Function which convert date to string
 function dateToString(date) {
     return date.toISOString().substring(0, 10);
 }
-
 //Function which returns today's date
 function today() {
     return dateToString(new Date());
 }
 
-let date = new Date();
-date = today();
-eatenWeek();
-
-function apportOMS(gender, weight, age) {
-    gender === "femme"
-        ? (document.getElementById("gender").innerHTML = "une femme")
-        : "un homme";
-    document.getElementById("weight").innerHTML = weight;
-    document.getElementById("age").innerHTML = age;
-    let energieGender = 2500;
-    if (gender === "homme") {
-        age <= 3 ? (energieGender = weight * 90) : energieGender;
-        age >= 3 && age <= 5 ? (energieGender = 1650) : energieGender;
-        age >= 5 && age <= 10 ? (energieGender = 2000) : energieGender;
-        age >= 10 && age <= 18 ? (energieGender = 2900) : energieGender;
-        age >= 19 && age <= 30 ? (energieGender = 2700) : energieGender;
-        age >= 31 && age <= 50 ? (energieGender = 2600) : energieGender;
-        age >= 51 && age <= 70 ? (energieGender = 2350) : energieGender;
-        age >= 71 ? (energieGender = 2200) : energieGender;
-    } else {
-        age <= 3 ? (energieGender = weight * 90) : energieGender;
-        age >= 3 && age <= 5 ? (energieGender = 1650) : energieGender;
-        age >= 5 && age <= 10 ? (energieGender = 2000) : energieGender;
-        age >= 10 && age <= 18 ? (energieGender = 2350) : energieGender;
-        age >= 19 && age <= 30 ? (energieGender = 2100) : energieGender;
-        age >= 31 && age <= 50 ? (energieGender = 2000) : energieGender;
-        age >= 51 && age <= 70 ? (energieGender = 1850) : energieGender;
-        age >= 71 ? (energieGender = 1750) : energieGender;
-    }
-    document.getElementById("energyOMS").innerHTML = energieGender * 7;
-    let ratioProtein = 0.8;
-    age <= 18 && age > 12 ? (ratioProtein = 1.2) : ratioProtein;
-    age <= 12 ? (ratioProtein = 2) : ratioProtein;
-    age >= 60 ? (ratioProtein = 1) : ratioProtein;
-    document.getElementById("proteinOMS").innerHTML = (
-        ratioProtein *
-        7 *
-        weight
-    ).toFixed(1);
-    document.getElementById("glucidOMS").innerHTML = (
-        energieGender / 8
-    ).toFixed(1);
-    document.getElementById("lipidOMS").innerHTML = (
-        (energieGender * 0.35) /
-        9
-    ).toFixed(1);
-    document.getElementById("fatOMS").innerHTML = (26 / 2000) * energieGender;
-    document.getElementById("sugarOMS").innerHTML = 25 * 7;
-    document.getElementById("fibreOMS").innerHTML = 35 * 7;
-    document.getElementById("cholesterolOMS").innerHTML = 300;
-    document.getElementById("saltOMS").innerHTML = 35;
+function apportOMS(id) {
+    $.ajax({
+        method: "POST",
+        url: "../backend/user.php",
+        data: {
+            type: "infoOMS",
+            id: id,
+        },
+    }).done((data) => {
+        const infoOMS = JSON.parse(data);
+        const currentYear = new Date().getFullYear();
+        const gender = infoOMS.gender;
+        const weight = infoOMS.weight;
+        const age = currentYear - infoOMS.born;
+        gender === "femme"
+            ? (document.getElementById("gender").innerHTML = "une femme")
+            : "un homme";
+        document.getElementById("weight").innerHTML = weight;
+        document.getElementById("age").innerHTML = age;
+        let energieGender = 2500;
+        if (gender === "homme") {
+            age <= 3 ? (energieGender = weight * 90) : energieGender;
+            age >= 3 && age <= 5 ? (energieGender = 1650) : energieGender;
+            age >= 5 && age <= 10 ? (energieGender = 2000) : energieGender;
+            age >= 10 && age <= 18 ? (energieGender = 2900) : energieGender;
+            age >= 19 && age <= 30 ? (energieGender = 2700) : energieGender;
+            age >= 31 && age <= 50 ? (energieGender = 2600) : energieGender;
+            age >= 51 && age <= 70 ? (energieGender = 2350) : energieGender;
+            age >= 71 ? (energieGender = 2200) : energieGender;
+        } else {
+            age <= 3 ? (energieGender = weight * 90) : energieGender;
+            age >= 3 && age <= 5 ? (energieGender = 1650) : energieGender;
+            age >= 5 && age <= 10 ? (energieGender = 2000) : energieGender;
+            age >= 10 && age <= 18 ? (energieGender = 2350) : energieGender;
+            age >= 19 && age <= 30 ? (energieGender = 2100) : energieGender;
+            age >= 31 && age <= 50 ? (energieGender = 2000) : energieGender;
+            age >= 51 && age <= 70 ? (energieGender = 1850) : energieGender;
+            age >= 71 ? (energieGender = 1750) : energieGender;
+        }
+        document.getElementById("energyOMS").innerHTML = energieGender * 7;
+        let ratioProtein = 0.8;
+        age <= 18 && age > 12 ? (ratioProtein = 1.2) : ratioProtein;
+        age <= 12 ? (ratioProtein = 2) : ratioProtein;
+        age >= 60 ? (ratioProtein = 1) : ratioProtein;
+        document.getElementById("proteinOMS").innerHTML = (
+            ratioProtein *
+            7 *
+            weight
+        ).toFixed(1);
+        document.getElementById("glucidOMS").innerHTML = (
+            energieGender / 8
+        ).toFixed(1);
+        document.getElementById("lipidOMS").innerHTML = (
+            (energieGender * 0.35) /
+            9
+        ).toFixed(1);
+        document.getElementById("fatOMS").innerHTML =
+            (26 / 2000) * energieGender;
+        document.getElementById("sugarOMS").innerHTML = 25 * 7;
+        document.getElementById("fibreOMS").innerHTML = 35 * 7;
+        document.getElementById("cholesterolOMS").innerHTML = 300;
+        document.getElementById("saltOMS").innerHTML = 35;
+    });
 }
 
 function getGoal(id) {
@@ -338,4 +349,10 @@ function cancel() {
     document.querySelector(".contentDiv").style.opacity = 1;
     document.getElementById("addGoalWrapper").innerHTML = "";
 }
-apportOMS("homme", 75, 21);
+
+async function main() {
+    await getGoal(id);
+    await apportOMS(id);
+    await eatenWeek();
+}
+main();
